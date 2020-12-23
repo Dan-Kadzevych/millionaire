cc22d8fbc5b7f2cc57755317053c76cb5b62693b
Dan Kadzevych <devteam@mplatform.com>
2020-12-23 23:30:27 +0200

    Added Is Loading Reducer

--- src/store/game/reducer.js
+++ src/store/game/reducer.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 
 import createReducer from 'utils/redux/createReducer';
 import {
-  // GET_GAME_CONFIG_REQUEST,
+  GET_GAME_CONFIG_REQUEST,
   GET_GAME_CONFIG_SUCCESS,
-  // GET_GAME_CONFIG_FAILURE,
+  GET_GAME_CONFIG_FAILURE,
 } from './types';
 
 const initialState = {
+  isLoading: true,
   questions: {
     byId: {},
     allIds: {},
@@ -17,6 +18,14 @@ const initialState = {
     allIds: {},
   },
 };
+
+function finishLoading() {
+  return false;
+}
+
+function startLoading() {
+  return true;
+}
 
 function getQuestionsSuccess(
   state = initialState.questions,
@@ -46,22 +55,34 @@ function getAnswersSuccess(
   return { ...state, byId: answersById, allIds: answerIds };
 }
 
+const isLoadingHandlers = {
+  [GET_GAME_CONFIG_REQUEST]: startLoading,
+  [GET_GAME_CONFIG_SUCCESS]: finishLoading,
+  [GET_GAME_CONFIG_FAILURE]: finishLoading,
+};
+
 const questionHandlers = {
   [GET_GAME_CONFIG_SUCCESS]: getQuestionsSuccess,
 };
+
+const answerHandlers = {
+  [GET_GAME_CONFIG_SUCCESS]: getAnswersSuccess,
+};
+
+const isLoadingReducer = createReducer(
+  initialState.isLoading,
+  isLoadingHandlers,
+);
 
 const questionsReducer = createReducer(
   initialState.questions,
   questionHandlers,
 );
 
-const answerHandlers = {
-  [GET_GAME_CONFIG_SUCCESS]: getAnswersSuccess,
-};
-
 const answersReducer = createReducer(initialState.answers, answerHandlers);
 
 export default combineReducers({
+  isLoading: isLoadingReducer,
   questions: questionsReducer,
   answers: answersReducer,
 });
